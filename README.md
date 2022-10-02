ef8a85c6b6f849b54a43c8ba98dfee3a54259675
Piyush <Piyush31009@gmail.com>
2022-10-02 19:32:04 +0530

    guessnumber

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
  the mini project
 1. Dice Roll
+2. Guess the number
---
+++ guessanumber.py
@@ -0,0 +1,13 @@
+import random
+guess=random.randint(0,100)
+a=0
+for i in range(0,3):
+    user=int(input("enter the number you think will get randomly from 0 to 99: "))
+    if user==guess:
+        a=1
+        print("Hurray!!! you guess the right number..")
+    else:
+        print("Try Again!! you can do it")
+
+if a==0:
+    print("Better luck next time and the number you need to guess is ", guess)
